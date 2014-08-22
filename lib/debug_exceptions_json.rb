5148e9a1c5c93b1f7a170cc338f647a71a7a2c5f
Taiki ONO <taiks.4559@gmail.com>
2014-08-22 13:27:00 +0900

    Implement rack app

--- lib/debug_exceptions_json.rb
+++ lib/debug_exceptions_json.rb
@@ -1,2 +1,49 @@
 module DebugExceptionsJson
+  class RackApp
+    DEFAULT_BUILDER = -> (exception, env) {
+      [
+        '500',
+        { 'Content-Type' => 'application/json' },
+        [
+          {
+            error: {
+              message: exception.message,
+              backtrace: exception.backtrace,
+            },
+          }.to_json
+        ],
+      ]
+    }
+
+    # app - A rack app
+    # response_builder - A Proc which builds response
+    #
+    # response_builder takes 2 parameters:
+    #   exception - An exception object
+    #   env - A Hash
+    #
+    def initialize(app, response_builder = DEFAULT_BUILDER)
+      @app = app
+      @response_builder = response_builder
+    end
+
+    def call(env)
+      @app.call(env)
+    rescue Exception => exception
+      raise exception unless show_exception?(env)
+      raise exception unless response_with_json?(env)
+
+      @response_builder.call(exception, env)
+    end
+
+    private
+
+    def show_exception?(env)
+      env['action_dispatch.show_exceptions'] && env['action_dispatch.show_detailed_exceptions']
+    end
+
+    def response_with_json?(env)
+      env['HTTP_ACCEPT'] =~ /application\/json/
+    end
+  end
 end
